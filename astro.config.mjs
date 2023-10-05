11a91f2a98052156a409207253bdd2b7d2265a9e
Lorenzo Greco <lorenzo.rev@gmail.com>
2023-10-05 17:13:12 +0200

    fix

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,6 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
     site: 'https://revh.github.io',
-    //base: '/revh.github.io',
+    base: '/website',
     output: 'static',
 });
